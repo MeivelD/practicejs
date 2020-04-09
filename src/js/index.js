aac20c6feebadd1c966540d455c0754fb09f3754
mdmeivel@gmail.com <mdmeivel@gmail.com>
2020-04-09 19:22:40 +0530

    AC-2: added handling for compare two methods.

--- src/js/index.js
+++ src/js/index.js
@@ -14,33 +14,46 @@ class PracticeArray {
     }
 
     compareTwoArray(firstArray, secondArray) {
-        let biggerArray = []
-        let length = firstArray.length;
-        if (firstArray.length < secondArray.length) {
-            length = secondArray.length;
-            for (var i = firstArray.length; i < secondArray.length; i++) {
-                firstArray.push(0);
-            }
-        } else {
-            for (var i = secondArray.length; i < firstArray.length; i++) {
-                secondArray.push(0);
-            }
-
-        }
-        for (var i = 0; i < length; i++) {
-            if (firstArray[i] <= secondArray[i]) {
-                biggerArray.push(secondArray[i]);
+        try {
+            let biggerArray = []
+            let length = firstArray.length;
+            if (firstArray.length < secondArray.length) {
+                length = secondArray.length;
+                for (var i = firstArray.length; i < secondArray.length; i++) {
+                    firstArray.push(0);
+                }
             } else {
-                biggerArray.push(firstArray[i]);
+                for (var i = secondArray.length; i < firstArray.length; i++) {
+                    secondArray.push(0);
+                }
             }
+            for (var i = 0; i < length; i++) {
+                let firstArrayValue = firstArray[i];
+                let secondArrayValue = secondArray[i];
+                if (isNaN(firstArrayValue)) {
+                    throw firstArrayValue + " is not a number.";
+                }
+                if (isNaN(secondArrayValue)) {
+                    throw secondArrayValue + " is not a number.";
+                }
+            }
+            for (var i = 0; i < length; i++) {
+                if (firstArray[i] <= secondArray[i]) {
+                    biggerArray.push(secondArray[i]);
+                } else {
+                    biggerArray.push(firstArray[i]);
+                }
+            }
+            return biggerArray;
+        } catch (error) {
+            alert(error);
+        } finally {
+            alert("Re-evaluate the given input(s).");
         }
-
-        return biggerArray;
     }
 }
-
 let arr = new PracticeArray();
-let firstArr = [1, 3, 5, 6],
+let firstArr = [1, 3, "a", 6],
     secondArr = [2, 4, 3, 1, 5]
 let biggerArr = arr.compareTwoArray(firstArr, secondArr);
 document.getElementById("demo").innerHTML = biggerArr;
